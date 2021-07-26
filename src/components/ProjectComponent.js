09f924a7037360cce76250c6e9468f2a6ccc7cd5
HeartyRhombus <kaylahmg@gmail.com>
2021-07-26 12:44:43 -0700

    stub out initial project cards & styling

--- src/components/ProjectComponent.js
+++ src/components/ProjectComponent.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
+import { Card, CardGroup } from 'react-bootstrap';
+import placeholder from '../images/abstract-q-g-250-125-2.jpg'
 
 class ProjectComponent extends Component {
     render() {
         return (
             <div>
-                Project Component
+                <CardGroup>
+                    <Card>
+                        <Card.Img variant="top" src={placeholder} />
+                        <Card.Body>
+                            <Card.Title> Test 1 </Card.Title>
+                            <Card.Text> Test Description Placeholder </Card.Text>
+                        </Card.Body>
+                    </Card>
+
+                    <Card>
+                        <Card.Img variant="top" src={placeholder} />
+                        <Card.Body>
+                            <Card.Title> Test 2 </Card.Title>
+                            <Card.Text> Test Description Placeholder </Card.Text>
+                        </Card.Body>
+
+                    </Card>
+                    <Card>
+                        <Card.Img variant="top" src={placeholder} />
+                        <Card.Body>
+                            <Card.Title> Test 3 </Card.Title>
+                            <Card.Text> Test Description Placeholder </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </CardGroup>
             </div>
         );
     }
--- src/containers/ProjectsContainer.js
+++ src/containers/ProjectsContainer.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import ProjectComponent from '../components/ProjectComponent';
 
 class ProjectsContainer extends Component {
     render() {
         return (
             <div>
                 Projects Container
+                <ProjectComponent />
             </div>
         );
     }
