cd898d60ede30c4e7bf9541c11c8e8eb25ce0bca
HeartyRhombus <kaylahmg@gmail.com>
2021-07-26 10:45:27 -0700

    add navbar & navbar styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import './App.css';
+import NavBar from './components/NavBar'
 import AboutContainer from './containers/AboutContainer';
 import ProjectsContainer from './containers/ProjectsContainer';
 import ContactContainer from './containers/ContactContainer';
 
 function App() {
   return (
-    <div className="App">
-      <AboutContainer/>
-      <ProjectsContainer/>
-      <ContactContainer/>
+    <div id="home" className="App">
+      <div className="NavBar">
+        <NavBar/>
+      </div>
+      <div>
+        <AboutContainer/>
+      </div>
+      <div id="projects">
+        <ProjectsContainer/>
+      </div>
+      <div id="contact">
+        <ContactContainer/>
+      </div>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,41 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { Nav, Navbar } from 'react-bootstrap'
 
 class NavBar extends Component {
     render() {
+
+        const scrollToHome = () => {
+            document.querySelector('#home').scrollIntoView()
+        }
+
+        const scrollToProj = () => {
+            document.querySelector('#projects').scrollIntoView()
+        }
+
+        const scrollToContact = () => {
+            document.querySelector('#contact').scrollIntoView()
+        }
+
         return (
-            <div>
-                Navigation Bar
-            </div>
+            <Navbar
+                expand="md"
+                className="justify-content-end"
+            >
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav>
+                        <Nav.Link onClick={scrollToHome}  >
+                            Home
+                        </Nav.Link>
+                        <Nav.Link onClick={scrollToProj}>
+                            Projects
+                        </Nav.Link>
+                        <Nav.Link onClick={scrollToContact}>
+                            Contact Me
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
         );
     }
 }
