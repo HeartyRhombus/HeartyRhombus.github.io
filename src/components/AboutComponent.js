1508091b0b6bd4713191c4256d1dfecbc4f6eb19
HeartyRhombus <kaylahmg@gmail.com>
2021-07-26 10:44:59 -0700

    add about me text

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -3,9 +3,11 @@ import React, { Component } from 'react';
 class AboutComponent extends Component {
     render() {
         return (
-            <>
-                About Component
-            </>
+            <div id="me_description">
+                I'm a recent graduate from Flatiron School's Full Time Online Software Engineering Program! I have a background in retail and customer service within the travel and home improvment industries.
+                <br/>
+                I love learning new things, and when I'm not coding I like to spend time in my garden, woodworking, restoring classic cars, or with my family and friends!
+            </div>
         );
     }
 }
