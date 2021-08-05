1b8ec977f0aef80046934205828849ab161d9944
HeartyRhombus <kaylahmg@gmail.com>
2021-08-05 15:50:33 -0700

    add link to resume & update icons

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import resume from '../images/resume-png-resume-icon-1600.png'
 import liLogo from '../images/linkedin-black-icon.png'
 import githubLogo from '../images/GitHub-Mark-120px-plus.png'
 import medLogo from '../images/Medium-Symbol-Black-RGB@4x.png'
@@ -11,33 +12,42 @@ class ContactComponent extends Component {
                 Please feel free to reach out to me here:
                 <br/>
                 <br/>
-                <a href="">
+                <a href="https://docs.google.com/document/d/1f9mlikm--E2WgOVrZtntlICwVWWbh6HR9WSc0VXk-zM/edit?usp=sharing" target="_blank">
                     <img
+                        className="contact-icon"
+                        src={resume}
+                        title="Resume"
+                        alt="Resume"
+                    />
+                </a>
+                <a href="https://www.linkedin.com/in/KaylaBudzeak" target="_blank">
+                    <img
+                        className="contact-icon"
                         src={liLogo}
-                        height="120px"
                         title="LinkedIn"
                         alt="LinkedIn"
                     />
                 </a>
-                <a href="">
+                <a href="https://www.github.com/HeartyRhombus" target="_blank">
                     <img
+                        className="contact-icon"
                         src={githubLogo}
                         title="GitHub"
                         alt="GitHub"
                     />
                 </a>
-                <a href="">
+                <a href="https://HeartyRhombus.medium.com" target="_blank">
                     <img
+                        className="contact-icon"
                         src={medLogo}
-                        height="120px"
                         title="Blog"
                         alt="Blog"
                     />
                 </a>
-                <a href="mailto:kayla.budzeak@icloud.com">
+                <a href="mailto:kayla.budzeak@icloud.com" target="_blank">
                     <img
+                        className="contact-icon"
                         src={emLogo}
-                        height="120px"
                         title="Email Me"
                         alt="Email"
                     />
