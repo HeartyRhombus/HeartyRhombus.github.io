32c403d10e80dc28a93a5fbc3b1b5e96ff0c823b
HeartyRhombus <kaylahmg@gmail.com>
2021-08-05 13:35:09 -0700

    add & import react player library for project videos

--- src/components/ProjectComponent.js
+++ src/components/ProjectComponent.js
@@ -3,6 +3,7 @@ import { Card, CardGroup } from 'react-bootstrap';
 import placeholder from '../images/abstract-q-g-250-125-2.jpg'
 import github from '../images/GitHub-Mark-32px.png'
 import youtube from '../images/youtube_social_icon_red.png'
+import ReactPlayer from "react-player"
 
 class ProjectComponent extends Component {
     render() {
@@ -10,28 +11,31 @@ class ProjectComponent extends Component {
             <div>
                 <CardGroup>
                     <Card>
-                        <Card.Img variant="top" src={placeholder} />
+                        <Card.Header>
+                            <ReactPlayer
+                                className="projDemoVid"
+                                url="https://youtu.be/AQkeDlNPZko"
+                            />
+                        </Card.Header>
                         <Card.Body>
                             <Card.Title> Travel Bucket List App </Card.Title>
                             <Card.Text> Test Description Placeholder </Card.Text>
                         </Card.Body>
                         <Card.Footer>
-                            <a href="https://github.com/HeartyRhombus/travel_client" target="_blank">
+                            <a href="https://github.com/HeartyRhombus/travel_client" target="_blank" rel="noreferrer">
                                 <img src={github} alt="GitHub" />
                             </a>
-                            <a href="https://youtu.be/AQkeDlNPZko" target="_blank">
-                            <img class="proj_icons" src={youtube} alt="YouTube" />
+                            <a href="https://youtu.be/AQkeDlNPZko" target="_blank" rel="noreferrer">
+                            <img className="proj_icons" src={youtube} alt="YouTube" />
                             </a>
                         </Card.Footer>
                     </Card>
 
                     <Card>
                         <Card.Header>
-                            <iframe
+                            <ReactPlayer
                                 className="projDemoVid"
-                                src="https://youtube.com/embed/6YGZMibcDJQ?rel=0&amp;modestbranding=1&amp;loop=1"
-                                title="A video demonstration of my application built to be able to keep track of my library of books."
-                                allowFullScreen
+                                url="https://youtube.com/embed/6YGZMibcDJQ"
                             />
                         </Card.Header>
                         <Card.Body>
@@ -39,27 +43,32 @@ class ProjectComponent extends Component {
                             <Card.Text> Test Description Placeholder </Card.Text>
                         </Card.Body>
                         <Card.Footer>
-                            <a href="https://github.com/HeartyRhombus/javascript_project" target="_blank">
-                                <img class="proj_icons" src={github} alt="GitHub" />
+                            <a href="https://github.com/HeartyRhombus/javascript_project" target="_blank" rel="noreferrer">
+                                <img className="proj_icons" src={github} alt="GitHub" />
                             </a>
-                            <a href="https://youtu.be/6YGZMibcDJQ" target="_blank">
-                                <img class="proj_icons" src={youtube} alt="YouTube" />
+                            <a href="https://youtu.be/6YGZMibcDJQ" target="_blank" rel="noreferrer">
+                                <img className="proj_icons" src={youtube} alt="YouTube" />
                             </a>
                         </Card.Footer>
 
                     </Card>
                     <Card>
-                        <Card.Img variant="top" src={placeholder} />
+                        <Card.Header>
+                            <ReactPlayer
+                                className="projDemoVid"
+                                url="https://youtu.be/AQkeDlNPZko"
+                            />
+                        </Card.Header>
                         <Card.Body>
                             <Card.Title> World of Warcraft Character Tracker </Card.Title>
                             <Card.Text> Test Description Placeholder </Card.Text>
                         </Card.Body>
                         <Card.Footer>
-                            <a href="https://github.com/HeartyRhombus/rails_project" target="_blank" >
-                                <img class="proj_icons" src={github} alt="GitHub" />
+                            <a href="https://github.com/HeartyRhombus/rails_project" target="_blank"  rel="noreferrer">
+                                <img className="proj_icons" src={github} alt="GitHub" />
                             </a>
-                            <a href="https://youtu.be/uz3ygkNOf04" target="_blank">
-                                <img class="proj_icons" src={youtube} alt="YouTube" />
+                            <a href="https://youtu.be/uz3ygkNOf04" target="_blank" rel="noreferrer">
+                                <img className="proj_icons" src={youtube} alt="YouTube" />
                             </a>
                         </Card.Footer>
                     </Card>
