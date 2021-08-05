b8d3502d04e82a8a95b35c5d9a7ed1eba3639b7f
HeartyRhombus <kaylahmg@gmail.com>
2021-08-05 12:00:14 -0700

    correct spelling error

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -4,7 +4,7 @@ class AboutComponent extends Component {
     render() {
         return (
             <div id="me_description">
-                I'm a recent graduate from Flatiron School's Full Time Online Software Engineering Program! I have a background in retail and customer service within the travel and home improvment industries.
+                I'm a recent graduate from Flatiron School's Full Time Online Software Engineering Program! I have a background in retail and customer service within the travel and home improvement industries.
                 <br/>
                 I love learning new things, and when I'm not coding I like to spend time in my garden, woodworking, restoring classic cars, or with my family and friends!
             </div>
