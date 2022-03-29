ef6ea0c74ac53a366b2e13e520a55aa26b6243f8
Kayla Budzeak <68920883+HeartyRhombus@users.noreply.github.com>
2022-03-29 11:56:31 -0700

    update about section with new role

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -4,7 +4,7 @@ class AboutComponent extends Component {
     render() {
         return (
             <div id="me_description">
-                I'm a recent graduate from Flatiron School's Full Time Online Software Engineering Program! I have a background in retail and customer service within the travel and home improvement industries.
+                I am currently enjoying my first tech role and am a 2021 graduate of Flatiron School's Full Time Onlie Software Engineering Program. Prior to entering the tech space, my background included retail and customer service within the travel and home improvement industries.
                 <br/>
                 I love learning new things, and when I'm not coding I like to spend time in my garden, woodworking, restoring classic cars, or with my family and friends!
             </div>
