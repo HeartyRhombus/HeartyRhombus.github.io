aceb892129fa58e14bcbfa669b1daccf65c2c628
Kayla Budzeak <68920883+HeartyRhombus@users.noreply.github.com>
2023-03-05 20:04:44 -0800

    correct spelling error & add hobby

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,15 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from "react"
 
 class AboutComponent extends Component {
-    render() {
-        return (
-            <div id="me_description">
-                I am currently enjoying my first tech role and am a 2021 graduate of Flatiron School's Full Time Onlie Software Engineering Program. Prior to entering the tech space, my background included retail and customer service within the travel and home improvement industries.
-                <br/>
-                I love learning new things, and when I'm not coding I like to spend time in my garden, woodworking, restoring classic cars, or with my family and friends!
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div id="me_description">
+        I am currently enjoying my first tech role and am a 2021 graduate of
+        Flatiron School's Full Time Online Software Engineering Program. Prior
+        to entering the tech space, my background included retail and customer
+        service within the travel and home improvement industries.
+        <br />I love learning new things, and when I'm not coding I like to
+        spend time in my garden, baking, woodworking, restoring classic cars, or
+        with my family and friends!
+      </div>
+    )
+  }
 }
 
-export default AboutComponent;
+export default AboutComponent
