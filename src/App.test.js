5d74066f39c282cd635bc82d72f74e85ed6916e7
Kayla Budzeak <kaylahmg@gmail.com.com>
2026-04-20 17:06:22 -0700

    clear all files to start from scratch

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders about component', () => {
-  render(<App />)
-  const aboutSection = screen.getByText(/first tech role/i)
-  expect(aboutSection).toBeInTheDocument()
-})
