078a88ddf773d9f192d194b18d3a6029b00f5416
Kayla Budzeak <kaylahmg@gmail.com.com>
2026-05-04 14:43:10 -0700

    update previous about container to be home and add description

--- src/app/_containers/HomeContainer.js
+++ src/app/_containers/HomeContainer.js
@@ -1,15 +1,27 @@
 import Image from 'next/image'
 import profilePic from '../_images/headshot.jpg'
 
-export default function AboutContainer() {
+export default function HomeContainer() {
   return (
     <div>
       <div id="welcome_msg">
         <Image id="profile_pic" src={profilePic} alt="profile headshot" />
-        <br />
         Hi, I'm Kayla!
         <br />
         Welcome & thanks for visiting!
+      </div>
+      <div id="me_description">
+        I am a graduate of Flatiron School's Full Time Online Software
+        Engineering Program, and am enjoying my current Software Engineer role
+        within the insurance industry.
+        <br />
+        <br />
+        Prior to entering the tech space, my background included retail and
+        customer service within the travel and home improvement industries.
+        <br />
+        <br />I love learning new things, and when I'm not coding I enjoy
+        spending time with my daughter and husband, in my garden, baking, or
+        woodworking!
       </div>
     </div>
   )
