aa5c636b50304e5a79cb3e87173c83b77ed48aa6
Kayla Budzeak <kaylahmg@gmail.com.com>
2026-05-04 14:42:44 -0700

    add tech stack component

---
+++ src/app/_components/TechStack.js
@@ -0,0 +1,29 @@
+import Image from 'next/image'
+import gh from '../_images/tech_stack/github_logo.png'
+import js from '../_images/tech_stack/js_logo.png'
+import ts from '../_images/tech_stack/ts_logo.png'
+
+export default function TechStack() {
+  return (
+    <>
+      <h4 className="hr-title" id="tech-stack-title">
+        Current Technical Skills
+      </h4>
+      <div className="flex flex-row justify-center" id="tech_logos">
+        <Image
+          className="tech_icons"
+          src={js}
+          alt="JavaScript"
+          title="JavaScript"
+        />
+        <Image
+          className="tech_icons"
+          src={ts}
+          alt="TypeScript"
+          title="TypeScript"
+        />
+        <Image className="tech_icons" src={gh} alt="GitHub" title="GitHub" />
+      </div>
+    </>
+  )
+}
