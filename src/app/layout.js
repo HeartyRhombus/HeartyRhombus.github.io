b7644ea9a3bf4b2cee316c789a6f265f9e0ef0dd
Kayla Budzeak <kaylahmg@gmail.com.com>
2026-05-04 14:41:02 -0700

    update to use navigation component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,28 +1,19 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './globals.css'
+import Navigation from './_components/Navigation'
 
 export const metadata = {
   title: "Kayla B's Portfolio",
-  description: "An online portfolio showcasing my projects and skills.",
-};
+  description: 'An online portfolio showcasing my projects and skills.',
+}
 
 export default function RootLayout({ children }) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en">
+      <body className="min-h-full flex flex-col App">
+        <Navigation />
+        <main>{children}</main>
+      </body>
     </html>
-  );
+  )
 }
